98d2ee8eb092b1a84e31244f7ca2a8367a740d6a
esadabdullahkosedag <kaesoftware@gmail.com>
2025-11-24 10:46:01 +0300

    OpenCV image opening commands are added

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 import torch.nn as nn
 import numpy as np
+import cv2
 
+
+img = cv2.imread("image.jpg")
+dim = (600, 600)
+img = cv2.resize(img, dim, interpolation=cv2.INTER_AREA)
+cv2.namedWindow("Resized Image", cv2.WINDOW_NORMAL)
+cv2.imshow("Resized Image", img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 
 print(np.sqrt(16))
 
