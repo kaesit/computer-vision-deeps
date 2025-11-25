6fc2025f27b814c3fb8f63ee89eea2bbc52f0300
esadabdullahkosedag <kaesoftware@gmail.com>
2025-11-25 12:16:55 +0300

    pytorch matrix and tensor multplication codes are added

--- pytorch/torch_functions.py
+++ pytorch/torch_functions.py
@@ -1,3 +1,12 @@
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import numpy as np
+
+
+a = torch.tensor([[1, 2, 3], [4, 5, 6], [7, 8, 9]], dtype=torch.float32)
+b = torch.tensor([[7, 8], [9, 10], [11, 12]], dtype=torch.float32)
+
+result = torch.mm(a, b)
+print(result)
 
